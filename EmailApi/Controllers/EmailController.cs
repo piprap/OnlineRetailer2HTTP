95104df27403bf49a5fcb773bd04cd89960c2674
Ostefar <56957832+Ostefar@users.noreply.github.com>
2023-10-08 13:18:35 +0200

    sidste async

--- EmailApi/Controllers/EmailController.cs
+++ EmailApi/Controllers/EmailController.cs
@@ -9,10 +9,12 @@ namespace EmailApi.Controllers
     public class EmailController : ControllerBase
     {
         [HttpPost]
-        public IActionResult SendEmail()
+        public async Task<IActionResult> SendEmail()
         {
             // If this was a real service, you would add code to send the email here.
             // In the mock service, we just return a success message.
+            await Task.Delay(500);
+
             Console.WriteLine("Email sent");
             return Ok($"Confirmation email has been sent");
         }
